890fb9ff09f29253d61cbf00dd5675a3bac9ab69
Nalyd59 <dy.crepin@gmail.com>
2023-06-30 16:14:28 +0200

    html css

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function select_img() {
-    
+function select_img(value) {
+    if (value === 'Johnson') {
+        document.getElementById('ok1').style.visibility = "visible";
+        document.getElementById('p1').style.visibility = "visible";
+        document.getElementById('p1').style.left = "140px";
+    }else if(value === 'Diesel'){
+        document.getElementById('ok2').style.visibility = "visible";
+        document.getElementById('p2').style.visibility = "visible";
+        document.getElementById('p2').style.left = "140px";
+    }else if(value === 'Imen'){
+        document.getElementById('ok3').style.visibility = "visible";
+        document.getElementById('p3').style.visibility = "visible";
+        document.getElementById('p3').style.left = "140px";
+    }else if(value === 'Jessica'){
+        document.getElementById('ok4').style.visibility = "visible";
+        document.getElementById('p4').style.visibility = "visible";
+        document.getElementById('p4').style.left = "140px";
+    }else if(value === 'Zita'){
+        document.getElementById('ok5').style.visibility = "visible";
+        document.getElementById('p5').style.visibility = "visible";
+        document.getElementById('p5').style.left = "140px";
+    }
 }
